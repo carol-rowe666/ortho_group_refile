1fe04f93e0a0415b23d9b8e1bf8d457175e92eed
carol-rowe666 <carol.rowe666@gmail.com>
2021-08-11 10:29:39 -0500

    changed path

--- get_dna_names.sh
+++ get_dna_names.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-FILES="/home/carol/Dropbox/Paul_and_Carol_Shared/Rijan_ortho/PAL2NAL/All_the_cds_files/Unpickled_cds_files/*.fa"
+FILES="/path/to/PAL2NAL/All_the_cds_files/Unpickled_cds_files/*.fa"
 
 for file in $FILES
 do
-	grep -n -H "^>" $file >> num_dna_names.txt
+	grep -H -n "^>" $file >> num_dna_names.txt
 done
 
